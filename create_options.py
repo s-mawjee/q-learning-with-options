ef91edf8c242aa9e742b2bacf1d2de2ec7093444
s-mawjee <21179950+s-mawjee@users.noreply.github.com>
2020-02-21 07:36:05 +0200

    change state representation

--- create_options.py
+++ create_options.py
@@ -1,8 +1,7 @@
 from utils.options import load_option, save_option
 import gym
 from option import QOption
-
-import envs.gridworld
+import environments.fourrooms.envs.fourrooms_env
 
 # Defining actions
 UP = 0
@@ -15,50 +14,46 @@ def create_options(env):
     policy_option_1 = {
 
         # ROOM 0
-        '(1, 1)': RIGHT, '(1, 2)': RIGHT, '(1, 3)': RIGHT, '(1, 4)': RIGHT, '(1, 5)': DOWN,
-        '(2, 1)': RIGHT, '(2, 2)': RIGHT, '(2, 3)': RIGHT, '(2, 4)': RIGHT, '(2, 5)': DOWN,
-        '(3, 1)': RIGHT, '(3, 2)': RIGHT, '(3, 3)': RIGHT, '(3, 4)': RIGHT, '(3, 5)': RIGHT,
-        '(4, 1)': RIGHT, '(4, 2)': RIGHT, '(4, 3)': RIGHT, '(4, 4)': RIGHT, '(4, 5)': UP,
-        '(5, 1)': RIGHT, '(5, 2)': RIGHT, '(5, 3)': RIGHT, '(5, 4)': RIGHT, '(5, 5)': UP,
+        0: RIGHT, 1: RIGHT, 2: RIGHT, 3: RIGHT, 4: DOWN,
+        10: RIGHT, 11: RIGHT, 12: RIGHT, 13: RIGHT, 14: DOWN,
+        20: RIGHT, 21: RIGHT, 22: RIGHT, 23: RIGHT, 24: RIGHT,
+        31: RIGHT, 32: RIGHT, 33: RIGHT, 34: RIGHT, 35: UP,
+        41: RIGHT, 42: RIGHT, 43: RIGHT, 44: RIGHT, 45: UP,
 
         # ROOM 1
-        '(1, 7)': DOWN, '(1, 8)': DOWN, '(1, 9)': DOWN, '(1, 10)': DOWN, '(1, 11)': DOWN,
-        '(2, 7)': DOWN, '(2, 8)': DOWN, '(2, 9)': DOWN, '(2, 10)': DOWN, '(2, 11)': DOWN,
-        '(3, 7)': DOWN, '(3, 8)': DOWN, '(3, 9)': DOWN, '(3, 10)': DOWN, '(3, 11)': DOWN,
-        '(4, 7)': DOWN, '(4, 8)': DOWN, '(4, 9)': DOWN, '(4, 10)': DOWN, '(4, 11)': DOWN,
-        '(5, 7)': DOWN, '(5, 8)': DOWN, '(5, 9)': DOWN, '(5, 10)': DOWN, '(5, 11)': DOWN,
-        '(6, 7)': RIGHT, '(6, 8)': RIGHT, '(6, 9)': DOWN, '(6, 10)': LEFT, '(6, 11)': LEFT,
+        5: DOWN, 6: DOWN, 7: DOWN, 8: DOWN, 9: DOWN,
+        15: DOWN, 16: DOWN, 17: DOWN, 18: DOWN, 19: DOWN,
+        26: DOWN, 27: DOWN, 28: DOWN, 29: DOWN, 30: DOWN,
+        36: DOWN, 37: DOWN, 38: DOWN, 39: DOWN, 40: DOWN,
+        46: DOWN, 47: DOWN, 48: DOWN, 49: DOWN, 50: DOWN,
+        52: RIGHT, 53: RIGHT, 54: DOWN, 55: LEFT, 56: LEFT,
 
         # ROOM 2
-        '(8, 7)': DOWN, '(8, 8)': DOWN, '(8, 9)': DOWN, '(8, 10)': DOWN, '(8, 11)': DOWN,
-        '(9, 7)': DOWN, '(9, 8)': DOWN, '(9, 9)': DOWN, '(9, 10)': DOWN, '(9, 11)': DOWN,
-        '(10, 7)': LEFT, '(10, 8)': LEFT, '(10, 9)': LEFT, '(10, 10)': LEFT, '(10, 11)': LEFT,
-        '(11, 7)': UP, '(11, 8)': UP, '(11, 9)': UP, '(11, 10)': UP, '(11, 11)': UP,
+        68: DOWN, 69: DOWN, 70: DOWN, 71: DOWN, 72: DOWN,
+        78: DOWN, 79: DOWN, 80: DOWN, 81: DOWN, 82: DOWN,
+        89: LEFT, 90: LEFT, 91: LEFT, 92: LEFT, 93: LEFT,
+        99: UP, 100: UP, 101: UP, 102: UP, 103: UP,
 
         # ROOM 3
-        '(7, 1)': RIGHT, '(7, 2)': UP, '(7, 3)': LEFT, '(7, 4)': LEFT, '(7, 5)': LEFT,
-        '(8, 1)': UP, '(8, 2)': UP, '(8, 3)': UP, '(8, 4)': UP, '(8, 5)': UP,
-        '(9, 1)': UP, '(9, 2)': UP, '(9, 3)': UP, '(9, 4)': UP, '(9, 5)': UP,
-        '(10, 1)': UP, '(10, 2)': UP, '(10, 3)': UP, '(10, 4)': UP, '(10, 5)': UP,
-        '(11, 1)': UP, '(11, 2)': UP, '(11, 3)': UP, '(11, 4)': UP, '(11, 5)': UP,
+        57: RIGHT, 58: UP, 59: LEFT, 60: LEFT, 61: LEFT,
+        63: UP, 64: UP, 65: UP, 66: UP, 67: UP,
+        73: UP, 74: UP, 75: UP, 76: UP, 77: UP,
+        83: UP, 84: UP, 85: UP, 86: UP, 87: UP,
+        94: UP, 95: UP, 96: UP, 97: UP, 98: UP,
 
-        '(3, 6)': RIGHT,
-        '(6, 2)': UP,
-        '(7, 9)': DOWN,
-        '(10, 6)': LEFT
+        25: RIGHT,
+        51: UP,
+        62: DOWN,
+        88: LEFT
 
     }
 
-    term_set = ['(3, 6)', '(7, 9)', '(10, 6)',
-                '(6, 2)']  # indexed by room
+    term_set = [25, 62, 88, 51]
 
     def policy_selection(policy, state):
         return policy[state]
 
     def termination_condition(termination_set, state):
-        # index = int(state[1])
-        # if state in termination_set:  # if stat in hall way then the termination_set changes by index of 1
-        #     index = (index + 1) % 3
 
         if state in termination_set:
             return True
@@ -74,49 +69,48 @@ def create_options(env):
 
     # Option 2 - anti clockwise hall way points
 
+    policy_option_2 = {
+        # ROOM 0
+        0: DOWN, 1: DOWN, 2: DOWN, 3: DOWN, 4: DOWN,
+        10: DOWN, 11: DOWN, 12: DOWN, 13: DOWN, 14: DOWN,
+        20: DOWN, 21: DOWN, 22: DOWN, 23: DOWN, 24: DOWN,
+        31: DOWN, 32: DOWN, 33: DOWN, 34: DOWN, 35: DOWN,
+        41: RIGHT, 42: DOWN, 43: LEFT, 44: LEFT, 45: LEFT,
+
+        # ROOM 1
+        5: DOWN, 6: DOWN, 7: DOWN, 8: DOWN, 9: DOWN,
+        15: DOWN, 16: DOWN, 17: DOWN, 18: DOWN, 19: DOWN,
+        26: LEFT, 27: LEFT, 28: LEFT, 29: LEFT, 30: LEFT,
+        36: UP, 37: UP, 38: UP, 39: UP, 40: UP,
+        46: UP, 47: UP, 48: UP, 49: UP, 50: UP,
+        52: UP, 53: UP, 54: UP, 55: UP, 56: UP,
+
+        # ROOM 2
+        68: RIGHT, 69: RIGHT, 70: UP, 71: LEFT, 72: LEFT,
+        78: UP, 79: UP, 80: UP, 81: UP, 82: UP,
+        89: UP, 90: UP, 91: UP, 92: UP, 93: UP,
+        99: UP, 100: UP, 101: UP, 102: UP, 103: UP,
+
+        # ROOM 3
+        57: DOWN, 58: DOWN, 59: DOWN, 60: DOWN, 61: DOWN,
+        63: DOWN, 64: DOWN, 65: DOWN, 66: DOWN, 67: DOWN,
+        73: DOWN, 74: DOWN, 75: DOWN, 76: DOWN, 77: DOWN,
+        83: RIGHT, 84: RIGHT, 85: RIGHT, 86: RIGHT, 87: RIGHT,
+        94: UP, 95: UP, 96: UP, 97: UP, 98: UP,
+
+        25: LEFT,
+        51: DOWN,
+        62: UP,
+        88: RIGHT
+
+    }
+
+    term_set = [51, 88, 62, 25]
+
     def termination_condition_2(termination_set, state):
         if state in termination_set:
             return True
         return False
-
-    policy_option_2 = {
-        # ROOM 0
-        '(1, 1)': DOWN, '(1, 2)': DOWN, '(1, 3)': DOWN, '(1, 4)': DOWN, '(1, 5)': DOWN,
-        '(2, 1)': DOWN, '(2, 2)': DOWN, '(2, 3)': DOWN, '(2, 4)': DOWN, '(2, 5)': DOWN,
-        '(3, 1)': DOWN, '(3, 2)': DOWN, '(3, 3)': DOWN, '(3, 4)': DOWN, '(3, 5)': DOWN,
-        '(4, 1)': DOWN, '(4, 2)': DOWN, '(4, 3)': DOWN, '(4, 4)': DOWN, '(4, 5)': DOWN,
-        '(5, 1)': RIGHT, '(5, 2)': DOWN, '(5, 3)': LEFT, '(5, 4)': LEFT, '(5, 5)': LEFT,
-
-        # ROOM 1
-        '(1, 7)': DOWN, '(1, 8)': DOWN, '(1, 9)': DOWN, '(1, 10)': DOWN, '(1, 11)': DOWN,
-        '(2, 7)': DOWN, '(2, 8)': DOWN, '(2, 9)': DOWN, '(2, 10)': DOWN, '(2, 11)': DOWN,
-        '(3, 7)': LEFT, '(3, 8)': LEFT, '(3, 9)': LEFT, '(3, 10)': LEFT, '(3, 11)': LEFT,
-        '(4, 7)': UP, '(4, 8)': UP, '(4, 9)': UP, '(4, 10)': UP, '(4, 11)': UP,
-        '(5, 7)': UP, '(5, 8)': UP, '(5, 9)': UP, '(5, 10)': UP, '(5, 11)': UP,
-        '(6, 7)': UP, '(6, 8)': UP, '(6, 9)': UP, '(6, 10)': UP, '(6, 11)': UP,
-
-        # ROOM 2
-        '(8, 7)': RIGHT, '(8, 8)': RIGHT, '(8, 9)': UP, '(8, 10)': LEFT, '(8, 11)': LEFT,
-        '(9, 7)': UP, '(9, 8)': UP, '(9, 9)': UP, '(9, 10)': UP, '(9, 11)': UP,
-        '(10, 7)': UP, '(10, 8)': UP, '(10, 9)': UP, '(10, 10)': UP, '(10, 11)': UP,
-        '(11, 7)': UP, '(11, 8)': UP, '(11, 9)': UP, '(11, 10)': UP, '(11, 11)': UP,
-        # ROOM 3
-        '(7, 1)': DOWN, '(7, 2)': DOWN, '(7, 3)': DOWN, '(7, 4)': DOWN, '(7, 5)': DOWN,
-        '(8, 1)': DOWN, '(8, 2)': DOWN, '(8, 3)': DOWN, '(8, 4)': DOWN, '(8, 5)': DOWN,
-        '(9, 1)': DOWN, '(9, 2)': DOWN, '(9, 3)': DOWN, '(9, 4)': DOWN, '(9, 5)': DOWN,
-        '(10, 1)': RIGHT, '(10, 2)': RIGHT, '(10, 3)': RIGHT, '(10, 4)': RIGHT,
-        '(10, 5)': RIGHT,
-        '(11, 1)': UP, '(11, 2)': UP, '(11, 3)': UP, '(11, 4)': UP, '(11, 5)': UP,
-
-        '(3, 6)': LEFT,
-        '(6, 2)': DOWN,
-        '(7, 9)': UP,
-        '(10, 6)': RIGHT
-
-    }
-
-    term_set = ['(6, 2)', '(10, 6)', '(7, 9)',
-                '(3, 6)']  # indexed by room
 
     option2 = QOption(env.observation_space.n, policy_option_2,
                       term_set, policy_selection,
@@ -133,7 +127,7 @@ def view_option_policy(env, option):
 
 
 if __name__ == '__main__':
-    env = gym.make("FourRooms-v1")
+    env = gym.make("G1-v0")
     create_options(env)
 
     # test load
